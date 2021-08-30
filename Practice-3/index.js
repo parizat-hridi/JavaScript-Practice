4053110a9641db0c9054eb34d2325f9406c5c98c
parizat-hridi <parizatbintekabir@gmail.com>
2021-08-30 17:10:43 +0600

    number method

---
+++ Practice-3/index.js
@@ -0,0 +1,15 @@
+var num = 123;
+document.write(num);
+
+var numberTwo = 12.5789;
+console.log(typeof numberTwo);
+console.log(numberTwo.toPrecision(3));
+console.log(numberTwo.toFixed(2));
+
+var name = "Parizat";
+console.log(typeof name);
+
+var boolean = true;
+console.log(typeof boolean);
+
+console.log(typeof Number(num));
